8a6c8af53ed9ee92cbd8ebe78f3caf65e0aecb4b
Ana Martinazzo <ana@mappa.cc>
2016-10-03 16:43:13 -0300

    adapt getApiKey function

--- src/Vindi.php
+++ src/Vindi.php
@@ -17,18 +17,12 @@ class Vindi
     public static $apiBase = 'https://app.vindi.com.br/api/v1/';
 
     /**
-     * The Environment variable name for API Key.
-     * @var string
-     */
-    public static $apiKeyEnvVar = 'VINDI_API_KEY';
-
-    /**
      * Get Vindi API Key from environment.
      * @return string
      */
     public function getApiKey()
     {
-        return getenv(static::$apiKeyEnvVar);
+        return \Config::get('vindi.api_key');
     }
 
     /**
